044c35fbb3f9c1034e56a54cd319a160b1601ebf
Morok22 <andrey.ponomarenko222@gmail.com>
2018-05-08 23:11:17 +0300

    create excursion.component

---
+++ survival-team/app/excursion/excursion.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excursion',
+  templateUrl: './excursion.component.html',
+  styleUrls: ['./excursion.component.css']
+})
+export class ExcursionComponent implements OnInit {
+  excursions = [
+    {name: 'ex 1'},
+    {name: 'ex 2'},
+    {name: 'ex 3'}
+  ]
+
+
+  ngOnInit() {
+  }
+
+}
